35a4023fe280edda78c25e69ba3b0e519b556018
Rohan Marangoly <rmarangoly@ucsb.edu>
2024-01-30 17:36:17 -0800

    added changes to controller and service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationController.java
@@ -20,6 +20,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 
+@Tag(name="Location info from nominatim.org")
 @Slf4j
 @RestController
 @RequestMapping("/api/locations")
